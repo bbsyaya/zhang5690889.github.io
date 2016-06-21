09c40683d58b518230760d3450be6c421153e067
zhang5690889 <robinzhang8803@gmail.com>
2016-06-21 03:45:52 +0000

    Updated two articles about green card

--- js/security.js
+++ js/security.js
@@ -1,12 +1,9 @@
-// This disables right click
-var message = "Function Disabled!";
-
+// This disables selection and right click
 function clickIE4() {
     if (event.button == 2) {
         return false;
     }
 }
-
 function clickNS4(e) {
     if (document.layers || document.getElementById && !document.all) {
         if (e.which == 2 || e.which == 3) {
@@ -22,69 +19,51 @@ else if (document.all && !document.getElementById) {
     document.onmousedown = clickIE4;
 }
 document.oncontextmenu = new Function("return false")
-
-// no selection 
-//disable mouse drag select start
-
 document.onselectstart = new Function('return false');
-
 function dMDown(e) { return false; }
-
 function dOClick() { return true; }
-
 document.onmousedown = dMDown;
-
 document.onclick = dOClick;
-
-$("#document").attr("unselectable", "on"); 
-
-//disable mouse drag select end
-
-//disable right click - context menu
-
 document.oncontextmenu = new Function("return false");
-
-
-//disable CTRL+A/CTRL+C through key board start
-
-//use this function
-
-
 function disableSelectCopy(e) {
-
-// current pressed key
-
     var pressedKey = String.fromCharCode(e.keyCode).toLowerCase();
-
     if (e.ctrlKey && (pressedKey == "c" || pressedKey == "x" || pressedKey == "v" || pressedKey == "a")) {
 
         return false;
-
     }
-
 }
-
 document.onkeydown = disableSelectCopy;
-
-
-//or use this function
-
-$(function () {
-
-    $(document).keydown(function (objEvent) {
+document.onkeydown = function (objEvent) {
 
         if (objEvent.ctrlKey || objEvent.metaKey) {
 
             if (objEvent.keyCode == 65 || objEvent.keyCode == 97) {
 
                 return false;
-
             }
-
-        //}
-
         }
 
-    });
+    };
 
-});
+// diable F12
+    document.onkeypress = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+           //alert('No F-12');
+            return false;
+        }
+    }
+    document.onmousedown = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+            //alert('No F-keys');
+            return false;
+        }
+    }
+document.onkeydown = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+            //alert('No F-keys');
+            return false;
+        }
+    }
